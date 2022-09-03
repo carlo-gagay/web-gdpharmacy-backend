877890f4369bea4cebe144866996884b26fffa5f
carlo-gagay <tabayagcarlo94@gmail.com>
2022-09-03 13:43:23 +0800

    Initialize

---
+++ src/main/java/com/simplesoft/gdpharmacy/GdpharmacyApplication.java
@@ -0,0 +1,13 @@
+package com.simplesoft.gdpharmacy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdpharmacyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GdpharmacyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplesoft/gdpharmacy/GdpharmacyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplesoft.gdpharmacy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GdpharmacyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
